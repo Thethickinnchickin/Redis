1af462e18e07168cc7ee22a85d72f8b90017ad2d
Matthew Reiley <mattreileydeveloper@gmail.com>
2025-06-29 19:34:06 -0700

    debugging

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,3 @@
 from app import create_app
 
 app = create_app()
-
-if __name__ == "__main__":
-    print("Mongo instance in User model:", getattr(app, 'mongo', None))
-    app.run(ssl_context='adhoc', debug=True)
